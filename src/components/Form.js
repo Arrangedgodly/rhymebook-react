befa44c5f11084f35343e3547c04abf34e86e066
Arrangedgodly <arrangedgodly@gmail.com>
2022-10-13 17:26:13 -0600

    starting to fix the errors

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,4 +1,22 @@
+import React from "react";
+
 function Form() {
+  const [theme, setTheme] = React.useState('');
+  const [body, setBody] = React.useState('');
+  const [query, setQuery] = React.useState(150);
+
+  function handleThemeChange(e) {
+    setTheme(e.target.value);
+  }
+
+  function handleBodyChange(e) {
+    setBody(e.target.value);
+  }
+
+  function handleQueryChange(e) {
+    setQuery(e.target.value);
+  }
+
   return (
     <form className="form note-form">
           <label htmlFor="title" className="form-label"
@@ -7,14 +25,18 @@ function Form() {
               name="title"
               type="text"
               className="form-input form-input-title"
+              value={theme}
+              onChange={handleThemeChange}
             />
           </label>
           <label htmlFor="body" className="form-label"
             >Lyrics
             <textarea
               name="body"
-              type="text-field"
+              type="text"
               className="form-input form-input-body"
+              value={body}
+              onChange={handleBodyChange}
             >
             </textarea>
           </label>
@@ -29,7 +51,6 @@ function Form() {
                   type="radio"
                   value="topic"
                   className="form-input-alt"
-                  checked
                 />
               </label>
               <p className="form-input-desc">
@@ -60,9 +81,11 @@ function Form() {
               min="10"
               max="200"
               step="10"
-              value="150"
+              value={query}
               className="form-input form-input-max"
+              onChange={handleQueryChange}
             />
+            {query}
           </label>
           <p className="form-label form-label-max"></p>
         </form>
--- src/components/Results.js
+++ src/components/Results.js
@@ -4,14 +4,46 @@ import Rhymes from './Rhymes';
 function Results() {
   return (
     <div className="results">
-      {
-        resultsSection.map(section => (
-          <Rhymes 
-            title={section.title}
-            suffix={section.suffix}
-          />
-        ))
-      }
+      <Rhymes 
+        title={resultsSection[0].title}
+        suffix={resultsSection[0].suffix}
+        key={resultsSection[0].suffix}
+      />
+      <Rhymes 
+        title={resultsSection[1].title}
+        suffix={resultsSection[1].suffix}
+        key={resultsSection[1].suffix}
+      />
+      <Rhymes 
+        title={resultsSection[2].title}
+        suffix={resultsSection[2].suffix}
+        key={resultsSection[2].suffix}
+      />
+      <Rhymes 
+        title={resultsSection[3].title}
+        suffix={resultsSection[3].suffix}
+        key={resultsSection[3].suffix}
+      />
+      <Rhymes 
+        title={resultsSection[4].title}
+        suffix={resultsSection[4].suffix}
+        key={resultsSection[4].suffix}
+      />
+      <Rhymes 
+        title={resultsSection[5].title}
+        suffix={resultsSection[5].suffix}
+        key={resultsSection[5].suffix}
+      />
+      <Rhymes 
+        title={resultsSection[6].title}
+        suffix={resultsSection[6].suffix}
+        key={resultsSection[6].suffix}
+      />
+      <Rhymes 
+        title={resultsSection[7].title}
+        suffix={resultsSection[7].suffix}
+        key={resultsSection[7].suffix}
+      />
     </div>
   );
 }
