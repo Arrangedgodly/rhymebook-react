1b0dcae5e9c263e4745bfd9dc0dd7adc3bc49642
Arrangedgodly <90996993+Arrangedgodly@users.noreply.github.com>
2023-01-03 01:42:20 -0700

    refactor: adding image to the header of the site

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,12 @@
+import logo from '../images/rhymebook_logo.webp';
+
 function Header() {
   return (
     <header className="header">
+      <img 
+        className='header-logo' 
+        src={logo}
+      />
       <h1 className="header-title">Rhymebook: The API Powered Notes App</h1>
     </header>
   );
